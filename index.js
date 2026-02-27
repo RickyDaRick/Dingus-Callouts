8c9995e1b1fa53a9bc6ab09e92e8750545eaf955
RickyDaRick <work.rcharles@gmail.com>
2026-02-27 16:47:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -516,7 +516,7 @@ function animate() {
     }
   }
   dots.forEach((dot) => {
-    if (dot.radius > 20000 && backH == false) {
+    if (dot.radius > 50000 && backH == false) {
       backH = true;
       window.location.reload();
     }
